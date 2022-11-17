826d1d6abf2d1cf10a39a4ba42ac50ee4b52db9e
aldous <aldous@dalegroup.net>
2022-11-17 14:21:21 +0100

    Full page design with no mobile responsive

---
+++ README.md
@@ -0,0 +1,5 @@
+# frontendpractice-abstract
+
+## Frontend practise in tailwind
+
+Replicate https://help.abstract.com/hc/en-us as part of https://www.frontendpractice.com/projects/abstract.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4C5FD5",
+        secondary: "#dadbf1",
+        black: "#000000",
+        white: "#ffffff",
+        blue: "#4C5FD5",
+      },
+    },
+  },
+  plugins: [],
+};
